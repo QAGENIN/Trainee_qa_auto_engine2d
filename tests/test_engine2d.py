5db4d1c90c95fc4505ceb483ca6a8d242f05b817
Roman Satymbaev <konev9494@mail.ru>
2023-10-22 14:35:59 +0300

    init

--- tests/test_engine2d.py
+++ tests/test_engine2d.py
@@ -1,31 +1,27 @@
-import time
-
 from engine.engine2d import Rectangle, Circle, Triangle, Engine2D
+
+engine = Engine2D(800, 600, "2D-движок")
 
 
 def test_rectangle_draw():
-    engine = Engine2D(800, 600, "2D-движок")
     shape = Rectangle('Прямоугольник', (255, 0, 0), (100, 200, 200, 100))
     engine.canvas.append(shape)
     engine.draw()
 
 
 def test_circle_draw():
-    engine = Engine2D(800, 600, "2D-движок")
     shape = Circle('Круг', (0, 255, 0), (400, 200, 100, 100))
     engine.canvas.append(shape)
     engine.draw()
 
 
 def test_triangle_draw():
-    engine = Engine2D(800, 600, "2D-движок")
     shape = Triangle('Треугольник', (0, 0, 255), (600, 200, 100, 100))
     engine.canvas.append(shape)
     engine.draw()
 
 
 def test_change_color():
-    engine = Engine2D(800, 600, "2D-движок")
     shape1 = Rectangle('Прямоугольник', (255, 0, 0), (100, 200, 200, 100))
     shape2 = Circle('Круг', (0, 255, 0), (400, 200, 100, 100))
 
@@ -39,7 +35,6 @@ def test_change_color():
 
 
 def test_delete_figure():
-    engine = Engine2D(800, 600, "2D-движок")
     shape1 = Rectangle('Прямоугольник', (255, 0, 0), (100, 200, 200, 100))
     shape2 = Circle('Круг', (0, 255, 0), (400, 200, 100, 100))
 
