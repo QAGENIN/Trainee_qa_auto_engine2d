fb3e13876e7272350105c28bfedcd27c1c7fdd5b
Roman Satymbaev <konev9494@mail.ru>
2023-10-23 10:59:29 +0300

    init

--- engine/engine2d.py
+++ engine/engine2d.py
@@ -4,42 +4,48 @@ import pygame
 class Shape:
     def __init__(self, text, color, position):
         self.text = text
-        self.color = color
+        self._color = color
         self.position = position
 
     def draw(self, screen):
         raise NotImplementedError
 
+    def _change_color(self, new_color):
+        self._color = new_color
+
+    def get_color(self):
+        return self._color
+
 
 class Rectangle(Shape):
     def draw(self, screen):
-        print("Drawing Rectangle: {}, color: {}, position: {}".format(self.text, self.color, self.position))
-        pygame.draw.rect(screen, self.color, self.position)
+        print("Drawing Rectangle: {}, color: {}, position: {}".format(self.text, self._color, self.position))
+        pygame.draw.rect(screen, self._color, self.position)
         font = pygame.font.Font(None, 20)
-        text_surface = font.render(self.text, True, self.color)
+        text_surface = font.render(self.text, True, self._color)
         screen.blit(text_surface, (10, 40))
 
 
 class Circle(Shape):
     def draw(self, screen):
-        print("Drawing Circle: {}, color: {}, position: {}".format(self.text, self.color, self.position))
-        pygame.draw.ellipse(screen, self.color, self.position)
+        print("Drawing Circle: {}, color: {}, position: {}".format(self.text, self._color, self.position))
+        pygame.draw.ellipse(screen, self._color, self.position)
         font = pygame.font.Font(None, 20)
-        text_surface = font.render(self.text, True, self.color)
+        text_surface = font.render(self.text, True, self._color)
         screen.blit(text_surface, (10, 60))
 
 
 class Triangle(Shape):
     def draw(self, screen):
-        print("Drawing Triangle: {}, color: {}, position: {}".format(self.text, self.color, self.position))
+        print("Drawing Triangle: {}, color: {}, position: {}".format(self.text, self._color, self.position))
         points = [
             (self.position[0] + self.position[2] / 2, self.position[1]),
             (self.position[0], self.position[1] + self.position[3]),
             (self.position[0] + self.position[2], self.position[1] + self.position[3])
         ]
-        pygame.draw.polygon(screen, self.color, points)
+        pygame.draw.polygon(screen, self._color, points)
         font = pygame.font.Font(None, 20)
-        text_surface = font.render(self.text, True, self.color)
+        text_surface = font.render(self.text, True, self._color)
         screen.blit(text_surface, (10, 80))
 
 
--- tests/test_engine2d.py
+++ tests/test_engine2d.py
@@ -4,61 +4,61 @@ from engine.engine2d import Rectangle, Circle, Triangle, Engine2D
 
 engine = Engine2D(800, 600, "2D-движок")
 
-shape1 = Rectangle('Прямоугольник', (255, 0, 0), (100, 200, 200, 100))
-shape2 = Circle('Круг', (0, 255, 0), (400, 200, 100, 100))
-shape3 = Triangle('Треугольник', (0, 0, 255), (600, 200, 100, 100))
+rectangle = Rectangle('Прямоугольник', (255, 0, 0), (100, 200, 200, 100))
+circle = Circle('Круг', (0, 255, 0), (400, 200, 100, 100))
+triangle = Triangle('Треугольник', (0, 0, 255), (600, 200, 100, 100))
 
 
 def test_rectangle_draw():
-    engine.canvas.append(shape1)
+    engine.canvas.append(rectangle)
     engine.draw()
     time.sleep(1)
 
-    assert shape1 in engine.canvas
+    assert rectangle in engine.canvas
     engine.canvas = []
 
 
 def test_circle_draw():
-    engine.canvas.append(shape2)
+    engine.canvas.append(circle)
     engine.draw()
     time.sleep(1)
 
-    assert shape2 in engine.canvas
+    assert circle in engine.canvas
     engine.canvas = []
 
 
 def test_triangle_draw():
-    engine.canvas.append(shape3)
+    engine.canvas.append(triangle)
     engine.draw()
     time.sleep(1)
 
-    assert shape3 in engine.canvas
+    assert triangle in engine.canvas
     engine.canvas = []
 
 
 def test_change_color():
-    engine.canvas.append(shape1)
-    engine.canvas.append(shape2)
-    engine.canvas.append(shape3)
+    engine.canvas.append(rectangle)
+    engine.canvas.append(circle)
+    engine.canvas.append(triangle)
 
-    shape1.color = (0, 0, 255)  # Изменяем цвет прямоугольника на синий
-    shape2.color = (255, 255, 0)  # Изменяем цвет круга на желтый
+    rectangle._change_color((0, 0, 255))  # Изменяем цвет прямоугольника на синий
+    circle._change_color((255, 255, 0))  # Изменяем цвет круга на желтый
 
     engine.draw()
     time.sleep(1)
 
-    assert shape1.color == (0, 0, 255), "Цвет прямоугольника не изменился на синий"
-    assert shape2.color == (255, 255, 0), "Цвет круга не изменился на желтый"
+    assert rectangle.get_color() == (0, 0, 255), "Цвет прямоугольника не изменился на синий"
+    assert circle.get_color() == (255, 255, 0), "Цвет круга не изменился на желтый"
     engine.canvas = []
 
 
 def test_delete_figure():
-    engine.canvas.append(shape1)
-    engine.canvas.append(shape2)
-    engine.canvas.append(shape3)
+    engine.canvas.append(rectangle)
+    engine.canvas.append(circle)
+    engine.canvas.append(triangle)
 
-    engine.canvas.remove(shape3)  # Удаляем прямоугольник из canvas
+    engine.canvas.remove(triangle)  # Удаляем прямоугольник из canvas
     engine.draw()
     time.sleep(1)
-    assert shape3 not in engine.canvas, "Прямоугольник не был удален из canvas"
+    assert triangle not in engine.canvas, "Прямоугольник не был удален из canvas"
 
