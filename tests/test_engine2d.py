60ef395fec34704f85833cb8b7162e9091259dcb
Roman Satymbaev <konev9494@mail.ru>
2023-10-24 10:58:07 +0300

    add test positions figure

--- tests/test_engine2d.py
+++ tests/test_engine2d.py
@@ -1,22 +1,11 @@
 import time
 
-from engine.engine2d import Rectangle, Circle, Triangle, Engine2D
+from engine.engine2d import Rectangle, Circle, Triangle
 
 rectangle = Rectangle('Прямоугольник', (255, 0, 0), (100, 200, 200, 100))
 circle = Circle('Круг', (0, 255, 0), (400, 200, 100, 100))
 triangle = Triangle('Треугольник', (0, 0, 255), (600, 200, 100, 100))
 
-
-def test_positions(engine_object):
-    engine_object.canvas.append(rectangle)
-    engine_object.canvas.append(circle)
-    engine_object.canvas.append(triangle)
-    engine_object.draw()
-    time.sleep(1)
-
-    assert rectangle.position == (100, 200, 200, 100)
-    assert circle.position == (400, 200, 100, 100)
-    assert triangle.position == (600, 200, 100, 100)
 
 def test_rectangle_draw(engine_object):
     engine_object.canvas.append(rectangle)
@@ -46,8 +35,8 @@ def test_change_color(engine_object):
     engine_object.canvas.append(circle)
     engine_object.canvas.append(triangle)
 
-    rectangle._change_color((0, 0, 255))  # Изменяем цвет прямоугольника на синий
-    circle._change_color((255, 255, 0))  # Изменяем цвет круга на желтый
+    rectangle._change_color((0, 0, 255))
+    circle._change_color((255, 255, 0))
 
     engine_object.draw()
     time.sleep(1)
@@ -56,12 +45,24 @@ def test_change_color(engine_object):
     assert circle.get_color() == (255, 255, 0), "Цвет круга не изменился на желтый"
 
 
+def test_positions(engine_object):
+    engine_object.canvas.append(rectangle)
+    engine_object.canvas.append(circle)
+    engine_object.canvas.append(triangle)
+    engine_object.draw()
+    time.sleep(1)
+
+    assert rectangle.position == (100, 200, 200, 100)
+    assert circle.position == (400, 200, 100, 100)
+    assert triangle.position == (600, 200, 100, 100)
+
+
 def test_delete_figure(engine_object):
     engine_object.canvas.append(rectangle)
     engine_object.canvas.append(circle)
     engine_object.canvas.append(triangle)
 
-    engine_object.canvas.remove(triangle)  # Удаляем прямоугольник из canvas
+    engine_object.canvas.remove(triangle)
     engine_object.draw()
     time.sleep(1)
     assert triangle not in engine_object.canvas, "Прямоугольник не был удален из canvas"
